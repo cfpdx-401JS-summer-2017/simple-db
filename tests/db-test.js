fe0f5afc8c98d81b511ab5641b4158aee854db42
Haley Tortorella <hdtort@gmail.com>
2017-07-14 15:12:46 -0700

    getAll test passed

--- tests/db-test.js
+++ tests/db-test.js
@@ -8,27 +8,29 @@ describe('db', () => {
 
     before(done => {
         rimraf(TEST_DIR, err => {
-            if(err) return done(err);
+            if (err) return done(err);
             else done();
         });
     });
 
     let alcohol = null;
     let ipa = null;
+    let drinks = [];
 
     before(done => {
         db.rootDir = TEST_DIR;
         db.createTable('alcohol', (err, store) => {
-            if(err) return done(err);
+            if (err) return done(err);
             alcohol = store;
             done();
         });
     });
 
     it('saves alcohol to the db', done => {
-        alcohol.save({type: 'beer', name: 'IPA'}, (err, savedBeer) => {
-            if(err) return done(err);
+        alcohol.save({ type: 'beer', name: 'IPA' }, (err, savedBeer) => {
+            if (err) return done(err);
             ipa = savedBeer;
+            drinks.push(ipa);
             assert.equal(savedBeer.type, 'beer');
             assert.equal(savedBeer.name, 'IPA');
             assert.ok(savedBeer._id);
@@ -38,13 +40,34 @@ describe('db', () => {
 
     it('gets alcohol by id', done => {
         alcohol.get(ipa._id, (err, beer) => {
-            if(err) return done(err);
-            console.log(beer);
+            if (err) return done(err);
             assert.equal(beer._id, ipa._id);
             done();
         });
 
     });
+
+    it('returns all of the alcohol objects', (done) => {
+        alcohol.save({type: 'hardA', name: 'gin'}, (err, saved) => {
+            if(err) return done(err);
+            drinks.push(saved);
+        } );
+        alcohol.getAll((err, alcoholArray) => {
+            if (err) return done(err);
+            assert.deepEqual(alcoholArray, drinks);
+            done();
+
+        });
+
+
+    });
+
+    // it('removes alcohol by id', done => {
+    //     alcohol.remove(ipa._id, (err, beer) => {
+    //         if (err) return done (err);
+    //         assert.equal(beer, {removed: true});
+    //     });
+    // });
 
 });
 
