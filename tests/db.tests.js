7e9c7b9ccaa2c75039cf6bd4648f869af27fa14a
Anwar Montasir <anwarmontasir@gmail.com>
2017-07-14 13:36:19 -0700

    successful test for saving two buildings

--- tests/db.tests.js
+++ tests/db.tests.js
@@ -44,7 +44,26 @@ describe('db', () => {
             assert.equal(animal.type, 'cat');
             assert.equal(animal.name, 'garfield');
             assert.ok(animal._id);
-            assert.ok(fs.readFileSync(path.join(TEST_DIR, 'animals/' + animal._id)));
+            assert.ok(fs.readFileSync(path.join(TEST_DIR, 'animals/' + animal._id + '.json')));
+
+            done();
+        });
+    });
+    it('saves buildings', done => {
+        buildings.save({ type: 'pretty', name: 'Falling Water'}, (err, building) => {
+            if (err) return done(err);
+            assert.equal(building.type, 'pretty');
+            assert.equal(building.name, 'Falling Water');
+            assert.ok(building._id);
+            assert.ok(fs.readFileSync(path.join(TEST_DIR, 'buildings/' + building._id + '.json')));
+        });
+        buildings.save({ type: 'skyscraper', name: 'Chrysler Building' }, (err, building) => {
+
+            if (err) return done(err);
+            assert.equal(building.type, 'skyscraper');
+            assert.equal(building.name, 'Chrysler Building');
+            assert.ok(building._id);
+            assert.ok(fs.readFileSync(path.join(TEST_DIR, 'buildings/' + building._id + '.json')));
 
             done();
         });
