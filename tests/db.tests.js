ad3cd3b4a3c26aa40757f0305d885432168afaf7
Mark Petrie <petrie.mark@gmail.com>
2017-07-13 20:47:37 -0700

    Finish 'saves animal' with 4 passing tests

--- tests/db.tests.js
+++ tests/db.tests.js
@@ -2,44 +2,53 @@ const assert = require('assert');
 const path = require('path');
 const rimraf = require('rimraf');
 const db = require('../lib/db');
+const fs = require('fs');
 
 describe('db', () => {
+
     const TEST_DIR = path.join(__dirname, 'test');
-   
+
     before(done => {
         rimraf(TEST_DIR, err => {
-            if(err) done(err);
+            if (err) done(err);
             else done();
         });
     });
 
     let animals = null;
-    
+
     before(done => {
         db.rootDir = TEST_DIR;
         db.createTable('animals', (err, store) => {
-            if(err) return done(err);
+            if (err) return done(err);
             animals = store;
             done();
         });
     });
 
     let buildings = null;
-    
+
     before(done => {
         db.rootDir = TEST_DIR;
         db.createTable('buildings', (err, store) => {
-            if(err) return done(err);
+            if (err) return done(err);
             buildings = store;
             done();
         });
     });
 
     it('saves animal', done => {
-        animals.save({ type: 'cat', name: 'garfield'}, (err, animal) => {
-            if(err) return done(err);
+        animals.save({ type: 'cat', name: 'garfield' }, (err, animal) => {
+
+            if (err) return done(err);
             assert.equal(animal.type, 'cat');
+            assert.equal(animal.name, 'garfield');
+            assert.ok(animal._id);
+            assert.ok(fs.readFileSync(path.join(TEST_DIR, 'animals/' + animal._id)));
+
             done();
         });
     });
 });
+
+// TODO: Mark - Anwar - Add a 'saves building' it block similar to 'saves animal'
