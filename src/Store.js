5869bd0a96940ae0cc2ffb2593a83dbe2b6b617b
Wendy Beck <webeck@gmail.com>
2017-07-22 14:03:12 -0700

    promisifying mainly finished but not all tests passing

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const promisify = require('util').promisify;
+const writeFile = promisify(fs.readFile);
+const readFile = promisify(fs.writeFile);
+const readdir = promisify(fs.readdir);
+const unlink = promisify(fs.unlink);
 const shortid = require('shortid');
 
 module.exports = class Store {
@@ -7,32 +12,27 @@ module.exports = class Store {
     this.dirName = dirName;
   }
 
-  save(obj, cb) {
+  save(obj) {
     obj._id = shortid.generate();
-    const filePath = path.join(this.dirName, obj._id + '.json');
-    fs.writeFile(filePath, JSON.stringify(obj), err => {
-      cb(err, obj);
-    });
+    const filePath = path.join(this.dirName, '/' + obj._id + '.json');
+    return writeFile(filePath, obj).then(() => obj);
   }
 
-  get(id, cb) {
+  get(id) {
     const filePath = path.join(this.dirName, id + '.json');
-    fs.readFile(filePath, (err, obj) => {
-      cb(err, JSON.parse(obj));
-    });
+    console.log(filePath);
+    return readFile(filePath).then(obj => obj);
   }
 
-  getAll(tableType, cb) {
-    const dirPath = path.join(this.dirName);
-    fs.readdir(dirPath, (err, files) => {
-      cb(err, files);
-    });
+  getAll() {
+    const filePath = path.join(this.dirName, '/');
+    console.log(filePath);
+    return readdir(filePath).then(files => files);
   }
 
-  remove(id, cb) {
+  remove(id) {
     const filePath = path.join(this.dirName, id + '.json');
-    fs.unlink(filePath, err => {
-      cb(err);
-    });
+    console.log(filePath);
+    unlink(filePath).then(status => status);
   }
 };
--- tests/db-function-tests.test.js
+++ tests/db-function-tests.test.js
@@ -1,75 +1,61 @@
+const promisify = require('util').promisify;
 const db = require('../src/db');
+const rimraf = promisify(require('rimraf'));
 const path = require('path');
 const chai = require('chai');
 const assert = chai.assert;
-const rimraf = require('rimraf');
 
 // table is the directory! ie., animals or buildings!
 // store is the individual obj - file in the directory!
 
 describe('test db functions', () => {
   const DATA_DIR = path.join(__dirname, 'data');
-
-  before(done => {
-    rimraf(DATA_DIR, err => {
-      if (err) done(err);
-      else done();
-    });
-  });
-
   let animals = null;
-  before(done => {
-    db.createTable('animals', (err, store) => {
-      if (err) done(err);
-      else {
-        animals = store;
-        done();
-      }
-    });
-  });
-
   let buildings = null;
-  before(done => {
-    db.createTable('buildings', (err, store) => {
-      if (err) done(err);
-      else {
-        buildings = store;
-        done();
-      }
-    });
+
+  before(() => {
+    return rimraf(DATA_DIR).then();
   });
 
-  it('saves new animal object', () => {
-    let newAnimal = { name: 'yolanda', color: 'pink' };
-    animals.save(newAnimal, (err, elephant) => {
-      if (err) throw err;
+  before(() => {
+    return db.createTable('animals').then(db => (animals = db));
+  });
+
+  before(() => {
+    return db.createTable('buildings').then(db => (buildings = db));
+  });
+  it.only('does nothing', () => {
+    console.log('it has done nothing');
+  });
+  it('tests callbacks', () => {
+    let newAnimal = { name: 'ruby', color: 'blue' };
+    return animals.save(newAnimal).then(elephant => {
+      assert.deepEqual(newAnimal.name, elephant.name);
+    });
+  }), it('saves new animal object', () => {
+    let newAnimal = { name: 'ruby', color: 'blue' };
+    return animals.save(newAnimal).then(elephant => {
       assert.deepEqual(newAnimal.name, elephant.name);
     });
   }), it('gets object from requested table by id', () => {
     let newBuilding = { name: 'skyscraper', numOfStories: '100' };
-    buildings.save(newBuilding, (err, skyscraper) => {
-      if (err) throw err;
-      buildings.get(skyscraper._id, (err, obj) => {
-        if (err) throw err;
+    return buildings.save(newBuilding).then(skyscraper => {
+      buildings.get(skyscraper._id).then(obj => {
+        assert.deepEqual(newBuilding.name, obj.name);
       });
-      assert.deepEqual(newBuilding, obj);
     });
   }), it('gets all objects from requested table', () => {
-    const tableType = 'animals';
-    animals.getAll(tableType, (err, stores) => {
-      if (err) throw err;
-      else {
-        if (stores.length < 1) assert.deepEqual([], stores);
-        else assert.isOk(stores);
-      }
+    return animals.getAll('animals').then(stores => {
+      console.log(stores);
+      if (stores.length < 1) assert.deepEqual([], stores);
+      else assert.isOk(stores);
     });
   }), it('removes the object by id', () => {
     let newAnimal = { name: 'josephina', color: 'orange' };
-    animals.save(newAnimal, (err, elephant) => {
-      if (err) throw err;
-      animals.remove(elephant._id, err => {
-        if (err) throw err;
-        else assert.notExists(elephant);
+    animals.save(newAnimal).then(elephant => {
+      animals.remove(elephant._id).then(status => {
+        console.log(status);
+        assert.notExists(elephant);
       });
     });
   });
